8416e318c40eb479706acfae0c0295f7a838a4f0
zhangqinglia <44956680+zhangqinglia@users.noreply.github.com>
2020-11-10 15:03:22 +0800

    Add files via upload
    ohhh

---
+++ build/precache-manifest.ce967f1b997bf94eb9b1e6bf95205f78.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feebf123fae5692c82b58c95fb03e3a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c69a3b6d99b1d7da2b9",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "0f88ada40cb997091e21",
+    "url": "/static/css/main.1fb4f76e.chunk.css"
+  },
+  {
+    "revision": "5c69a3b6d99b1d7da2b9",
+    "url": "/static/js/2.d6aa6b57.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.d6aa6b57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f88ada40cb997091e21",
+    "url": "/static/js/main.6ccf984a.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  },
+  {
+    "revision": "a0847c16bb718e89cb27e02cd7d5f9cd",
+    "url": "/static/media/banner.a0847c16.jpg"
+  },
+  {
+    "revision": "03f54d74e01b6fcc7d1b82cea8690237",
+    "url": "/static/media/carousel-1.03f54d74.jpg"
+  },
+  {
+    "revision": "4f0049a4c882f1cb9d836f7f48ec79a7",
+    "url": "/static/media/carousel-2.4f0049a4.jpg"
+  },
+  {
+    "revision": "fe28f5d1024a2f80de1bcaf897976641",
+    "url": "/static/media/carousel-3.fe28f5d1.jpg"
+  },
+  {
+    "revision": "92a82db2299e5306053c27ec4e0f766f",
+    "url": "/static/media/kefu.92a82db2.png"
+  },
+  {
+    "revision": "3ac29b182921c968b649b11e416381f5",
+    "url": "/static/media/shangpin-1.3ac29b18.jpg"
+  },
+  {
+    "revision": "d266653ad82903a5026c0acc20473cac",
+    "url": "/static/media/shangpin.d266653a.jpg"
+  },
+  {
+    "revision": "a517b05f83ad9b96cd90440ad8ab05d3",
+    "url": "/static/media/taobao.a517b05f.png"
+  },
+  {
+    "revision": "c4876f188a89703236169093dcf44ed3",
+    "url": "/static/media/xingxing.c4876f18.png"
+  }
+]);
